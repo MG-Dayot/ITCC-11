98c22cb8dff243b5aa22eb99eb781b5105dcecdd
https-n0connect10n <dayot.marygwyneth01@gmail.com>
2025-02-23 15:08:08 +0800

    add search file class

---
+++ Activity 2/SearchFile.java
@@ -0,0 +1,19 @@
+public class SearchFile {
+    private FileFoundListener listener;
+
+    void FileFoundListener(FileFoundListener listener) {
+        this.listener = listener;
+    }
+
+    void search(FileSystemNode node, String extension, String path) {
+        if (!node.isDirectroy && node.name.endsWith(extension)) {
+            if (listener != null) {
+                listener.onFileFound(path + node.name);
+            }
+        } else if (node.isDirectroy) {
+            for (FileSystemNode child : node.children) {
+                search(child, extension, path + node.name + "/")
+            }
+        }
+    }
+}
