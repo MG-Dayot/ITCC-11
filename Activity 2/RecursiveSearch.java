b1ad3b173b133299378018260d7d78a5d960ec87
https-n0connect10n <dayot.marygwyneth01@gmail.com>
2025-02-23 15:15:47 +0800

    add recursive search class

---
+++ Activity 2/RecursiveSearch.java
@@ -0,0 +1,13 @@
+public class RecursiveSearch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter directory path: ");
+        String path = input.nextLine();
+
+        System.out.println("Enter file extension to search: ")
+        String extension = input.nextLine();
+
+        
+    }
+}
