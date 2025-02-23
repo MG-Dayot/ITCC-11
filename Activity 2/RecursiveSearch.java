f34237f098d5cbd48b8af2b558e01c282be3eda0
https-n0connect10n <dayot.marygwyneth01@gmail.com>
2025-02-23 15:39:38 +0800

    edits, add

--- Activity 2/RecursiveSearch.java
+++ Activity 2/RecursiveSearch.java
@@ -8,6 +8,37 @@ public class RecursiveSearch {
         System.out.println("Enter file extension to search: ")
         String extension = input.nextLine();
 
-        
+        FileSystemNode root = new FileSystemNode(directory, true);
+        FileSystemNode file1 = new FileSystemNode("Activity1.java", false);
+        FileSystemNode file2 = new FileSystemNode("Activity2.java", false);
+        FileSystemNode subFolder1 = new FileSystemNode("Activities", true);
+        FileSystemNode file3 = new FileSystemNode("receipt.txt", false);
+        FileSystemNode file4 = new FileSystemNode("Readings.pdf", false);
+        FileSystemNode file5 = new FileSystemNode("code snippet.txt", false);
+        FileSystemNode subFolder2 = new FileSystemNode("Miscellaneous", true);
+
+        subFolder1.addChild(file1);
+        subFolder1.addChild(file2);
+        root.addChild(subFolder1);
+        subFolder2.addChild(file3);
+        root.addChild(file4);
+        subFolder2.addChild(file5);
+        root.addChild(subFolder2);
+
+        SearchFile search = new SearchFile();
+
+        try(PrintWriter writer = new PrintWriter(new FileWriter("searchResults.txt"))) {
+            search.setFileFoundListener(filePath -> {
+                System.out.println("File found: " + filePath);
+                writer.println(filePath);
+            })
+
+            System.out.println("\nSearching...");
+            search.search(root, extension, " ");
+            System.out.println("\nSearch completed. Results saved to searchResults.txt")
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+
     }
 }
